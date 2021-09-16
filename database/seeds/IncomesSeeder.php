5280e5deb2930936d4036d416fd21d8e22d3deae
nightfury99 <shauqighost99@gmail.com>
2021-09-16 11:41:44 +0800

    asdsad

--- database/seeds/IncomesSeeder.php
+++ database/seeds/IncomesSeeder.php
@@ -19,18 +19,22 @@ class IncomesSeeder extends Seeder
             ],
             [
                 'id' => 2,
-                'range' => 'Kurang RM5000'
+                'range' => 'RM1000 - RM5000'
             ],
             [
                 'id' => 3,
-                'range' => 'Kurang RM10000'
+                'range' => 'RM5000 - RM10000'
             ],
             [
                 'id' => 4,
-                'range' => 'Kurang RM15000'
+                'range' => 'RM10000 - RM15000'
             ],
             [
                 'id' => 5,
+                'range' => 'RM15000 - RM20000'
+            ],
+            [
+                'id' => 6,
                 'range' => 'Lebih RM20000'
             ]
         ]);
